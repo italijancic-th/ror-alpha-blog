d87e2e0a75d184ba6790f122ce2213efabcc7973
Ivan Talijancic <ivan.talijancic@teladochealth.com>
2023-02-06 18:08:39 -0300

    Add error handler on create for CRUD operations
        - Show errors on new form, after try to save and reject operations
          because of validation.
    
        - Use flash helper to show success menssages when a creation
          proccess y done!

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,14 +13,22 @@ class ArticlesController < ApplicationController
 
   # Create a new article
   def new
+    # In order to get access to errors on save operations and redirect to this form again
+    @article = Article.new
   end
 
   def create
     @article = Article.new(params.required(:article).permit(:title, :description))
     if @article.save
+      flash[:notice] = 'Article was created successfully.'
+      # Helpers usage to redirect to nother page
       redirect_to @article
     else
-      redirect_to new_article_path
+      # Two ways to re render new article form, in case of error saving
+      # redirect_to new_article_path
+      render :new, status: :unprocessable_entity
+      # NOTE: this is the way to do this of error handle on RoR 7
+      # new = name of template .erb to render
     end
     # binding.break
   end
